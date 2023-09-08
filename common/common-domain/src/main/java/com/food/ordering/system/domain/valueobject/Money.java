16348e13155a2a667658ccea96d3e9ed996fea51
SemihKokcu <semihkkc1@gmail.com>
2023-09-08 18:04:17 +0300

    order eventler ve OrderDomainService yazıldı

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 public class Money {
 
     private  final BigDecimal amount;
+    public  static final Money ZERO = new Money(BigDecimal.ZERO);
 
     public Money(BigDecimal amount) {
         this.amount = amount;
@@ -14,6 +15,10 @@ public class Money {
 
     public BigDecimal getAmount() {
         return amount;
+    }
+
+    public boolean isGreaterThanZero() {
+        return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
     }
 
     public  boolean isGreatherThan(Money money){
--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -5,19 +5,36 @@ import com.food.ordering.system.domain.valueobject.Money;
 import com.food.ordering.system.domain.valueobject.OrderId;
 import com.food.ordering.system.order.service.domain.valueobject.OrderItemId;
 
-//çekirdek kodda lombok kullanmıyoruz
 public class OrderItem extends BaseEntity<OrderItemId> {
     private OrderId orderId;
     private final Product product;
-    private final  int quantity;
-    private  final Money subTotal;
+    private final int quantity;
+    private final Money price;
+    private final Money subTotal;
+
+    void initializeOrderItem(OrderId orderId, OrderItemId orderItemId) {
+        this.orderId = orderId;
+        super.setId(orderItemId);
+    }
+
+    boolean isPriceValid() {
+        return price.isGreaterThanZero() &&
+                price.equals(product.getPrice()) &&
+                price.multiply(quantity).equals(subTotal);
+    }
 
     private OrderItem(Builder builder) {
         super.setId(builder.orderItemId);
         product = builder.product;
         quantity = builder.quantity;
+        price = builder.price;
         subTotal = builder.subTotal;
     }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
 
     public OrderId getOrderId() {
         return orderId;
@@ -31,6 +48,10 @@ public class OrderItem extends BaseEntity<OrderItemId> {
         return quantity;
     }
 
+    public Money getPrice() {
+        return price;
+    }
+
     public Money getSubTotal() {
         return subTotal;
     }
@@ -39,13 +60,10 @@ public class OrderItem extends BaseEntity<OrderItemId> {
         private OrderItemId orderItemId;
         private Product product;
         private int quantity;
+        private Money price;
         private Money subTotal;
 
         private Builder() {
-        }
-
-        public static Builder builder() {
-            return new Builder();
         }
 
         public Builder orderItemId(OrderItemId val) {
@@ -60,6 +78,11 @@ public class OrderItem extends BaseEntity<OrderItemId> {
 
         public Builder quantity(int val) {
             quantity = val;
+            return this;
+        }
+
+        public Builder price(Money val) {
+            price = val;
             return this;
         }
 
