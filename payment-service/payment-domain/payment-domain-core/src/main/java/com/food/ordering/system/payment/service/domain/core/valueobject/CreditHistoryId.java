97897719bb7e381ad2bef7bff49b9fa870911441
SemihKokcu <semihkkc1@gmail.com>
2023-09-11 11:44:12 +0300

    payment-service hexoganal added and domain entity,valuobjcets created

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/core/valueobject/CreditHistoryId.java
@@ -0,0 +1,2 @@
+package com.food.ordering.system.payment.service.domain.core.valueobject;public class CreditHistoryId {
+}
---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/core/valueobject/TransactionType.java
@@ -0,0 +1,4 @@
+package com.food.ordering.system.payment.service.domain.core.valueobject;
+
+public class TransactionType {
+}
