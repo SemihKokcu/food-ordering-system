be9e9130e526010681c848411b7478f935e18c06
SemihKokcu <semihkkc1@gmail.com>
2023-09-12 22:55:00 +0300

    outbox-pattern implemented in order-service and tested completed

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/config/OrderServiceConfigData.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/config/OrderServiceConfigData.java
@@ -8,9 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "order-service")
 public class OrderServiceConfigData {
-
     private String paymentRequestTopicName;
     private String paymentResponseTopicName;
-    private String restaurantApprovalTopicName;
-    private  String restaurantApprovalResponseName;
+    private String restaurantApprovalRequestTopicName;
+    private String restaurantApprovalResponseTopicName;
 }
